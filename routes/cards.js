c3c06ee9892530720872bf1ccb93a7d285f92ea6
MassloveDen <78202134+MassloveDen@users.noreply.github.com>
2023-07-06 14:56:37 +0000

    try to fix

--- routes/cards.js
+++ routes/cards.js
@@ -3,14 +3,14 @@ const cardsRouter = require('express').Router();
 const {
   getCards,
   createCards,
-  deleteCards,
+  deleteCard,
   putLike,
   removeLike,
 } = require('../controllers/cards');
 
 cardsRouter.get('/', getCards);
 cardsRouter.post('/', createCards);
-cardsRouter.delete('/:cardId', deleteCards);
+cardsRouter.delete('/:cardId', deleteCard);
 cardsRouter.put('/:cardId/likes', putLike);
 cardsRouter.delete('/:cardId/likes', removeLike);
 
